6e9b8664c4bc9b7174e963ac7febfa7007949ed7
Stanley Wang <to.stanley.wang4130@gmail.com>
2024-05-19 05:08:23 -0400

    sync

--- backend/server.mjs
+++ backend/server.mjs
@@ -17,7 +17,7 @@ async function chain_score(formatted_history, totalvolume, average_volume_per_se
     \n4) time between most recent transaction and earliest known transaction in seconds: ${differenceInSeconds}
     \n5) frequency: based upon data provided by the wallet history
     \n6) wallet size: assuming the wallet size is at most 1.5 times the total volume
-    \n.Provide a rating out of 10 for the following metrics to measure health of the wallet assuming a normal distribution unless otherwise specified for the metrics based on the average expected size of a NEAR wallet is 3000 NEAR and 
+    \n.Provide a rating out of 10 up to one decimal for the following metrics to measure health of the wallet assuming a normal distribution unless otherwise specified for the metrics based on the average expected size of a NEAR wallet is 3000 NEAR and 
     having an average 2.7 transactions in a day:
     \n a) Total Volume: total volume traded.
     \n b) Average Volume: taking into account the average volume per day and second
@@ -40,7 +40,7 @@ async function chain_score(formatted_history, totalvolume, average_volume_per_se
   return(completion.choices[0]);
 }
 
-app.get('/wallet', (req, res) => {
+app.get('/wallet', async (req, res) => {
   try {
 
   const data = testFunction('testnet'); // This is the user that needs to be changed based upon input from frontend
@@ -69,19 +69,5 @@ app.get('/wallet', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-  const data = testFunction('testnet'); // This is the user that needs to be changed based upon input from frontend
-    
-  let totalvolume = 0;
-  for (let i = 0; i < data.length; i++) {
-    totalvolume += data[i].amount;
-  }
-  let recent_transaction = new Date(data[0].timestamp);
-  let oldest_transaction = new Date(data[data.length - 1].timestamp);
-  let differenceInSeconds = (recent_transaction.getTime() - oldest_transaction.getTime()) / 1000;
-
-  let average_volume_per_second = totalvolume / differenceInSeconds;
-  let average_volume_per_day = average_volume_per_second * 86400;
-  const chainscore_jsonobj = JSON.parse(await(chain_score(data, average_volume_per_second, average_volume_per_day, differenceInSeconds)));
-
 
 });
